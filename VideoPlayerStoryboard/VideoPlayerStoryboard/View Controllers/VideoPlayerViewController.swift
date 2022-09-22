b24dc0b296ed028ed2a5e035ff9a81cb5f4d6e26
Eugene Lu <eugenelu9848@gmail.com>
2022-09-22 12:09:51 -0700

    Convert base project to Swift

---
+++ VideoPlayerStoryboard/VideoPlayerStoryboard/View Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VideoPlayerViewController.swift
+//  VideoPlayerStoryboard
+//
+//  Created by Eugene Lu on 2022-09-22.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Video Player"
+    }
+}
